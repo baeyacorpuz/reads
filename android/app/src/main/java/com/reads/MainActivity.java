1a9520cfd26ed17c96629e6a6697136bcc2b1210
Bea Aira Lee Corpuz <bcorpuz.cloudpanda@gmail.com>
2021-09-24 17:30:39 +0800

    [ fix ] react navigation

--- android/app/src/main/java/com/reads/MainActivity.java
+++ android/app/src/main/java/com/reads/MainActivity.java
@@ -1,6 +1,7 @@
 package com.reads;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "reads";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
